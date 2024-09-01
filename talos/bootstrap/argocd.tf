d0dfe0e7205a353256fb67595583659fad272536
Cel Pynenborg <cel.pynenborg@telenetgroup.be>
2024-09-01 22:23:34 +0200

    Add initial Argo Applications

---
+++ talos/bootstrap/argocd.tf
@@ -0,0 +1,29 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  repository = "oci://ghcr.io/argoproj/argo-helm"
+  chart      = "argo-cd"
+  version    = "7.5.0"
+
+  values = [
+    templatefile("${path.module}/templates/argocd/values.yaml", {
+      sshPrivateKey = indent(8, data.aws_secretsmanager_secret_version.argocd_ssh_key.secret_string)
+    })
+  ]
+}
+
+resource "kubernetes_manifest" "app_of_apps" {
+  manifest = yamldecode(templatefile("${path.module}/templates/app-of-apps.yaml", {}))
+
+  depends_on = [helm_release.argocd]
+}
+
+data "aws_secretsmanager_secret" "argocd_ssh_key" {
+  name = "homelab/gitops/ssh-private-key"
+}
+
+data "aws_secretsmanager_secret_version" "argocd_ssh_key" {
+  secret_id = data.aws_secretsmanager_secret.argocd_ssh_key.id
+}
---
+++ talos/bootstrap/provider.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.65.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "global-resources"
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "admin@cluster"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+terraform {
+  backend "s3" {
+    key     = "homelab/proxmox/kubernetes/bootstrap/terraform.tfstate"
+    bucket  = "pynenborg-home-terraform-eu-west-1"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
